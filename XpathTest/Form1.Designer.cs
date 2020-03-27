e4d97fd6734c70d7ba62a67ecf7911a94af4b5d6
KENNARD OWEN <1067865703@qq.com>
2020-03-27 16:54:39 +0800

    Add project files.

---
+++ XpathTest/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace XpathTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxXml = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtCustomer = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxXml
+            // 
+            this.textBoxXml.Location = new System.Drawing.Point(18, 38);
+            this.textBoxXml.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxXml.MaxLength = 99932767;
+            this.textBoxXml.Multiline = true;
+            this.textBoxXml.Name = "textBoxXml";
+            this.textBoxXml.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxXml.Size = new System.Drawing.Size(373, 344);
+            this.textBoxXml.TabIndex = 0;
+            this.textBoxXml.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1120, 414);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(402, 38);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(827, 344);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxValue.Location = new System.Drawing.Point(20, 592);
+            this.textBoxValue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxValue.MaxLength = 99932767;
+            this.textBoxValue.Multiline = true;
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.ReadOnly = true;
+            this.textBoxValue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxValue.Size = new System.Drawing.Size(1090, 170);
+            this.textBoxValue.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1120, 498);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "搜索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(20, 414);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.MaxLength = 99932767;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(1090, 85);
+            this.textBox3.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(1120, 618);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "查看";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 767);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1250, 30);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(36, 25);
+            this.toolStripStatusLabel1.Text = "OK";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "输入需要匹配的文件：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(367, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Xpath表达式（筛选结果），点击可现实具体Xpath";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 389);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(276, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "输入搜索内容，点击搜索，获取Xpath";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 569);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(463, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "显示Xpath，点击右边查看按钮，可查看通过该Xpath获取到的值";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1120, 684);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "清空数据";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtCustomer
+            // 
+            this.txtCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustomer.Location = new System.Drawing.Point(20, 509);
+            this.txtCustomer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCustomer.MaxLength = 99932767;
+            this.txtCustomer.Multiline = true;
+            this.txtCustomer.Name = "txtCustomer";
+            this.txtCustomer.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCustomer.Size = new System.Drawing.Size(1090, 55);
+            this.txtCustomer.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(1120, 554);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 35);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "自定义查找";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 797);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtCustomer);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.textBoxXml);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxXml;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtCustomer;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ XpathTest/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+using HtmlAgilityPack;
+
+namespace XpathTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hd.LoadHtml(textBoxXml.Text);
+            Thread th = new Thread(NewMethod);
+            th.Start();
+        }
+
+        private delegate void Dg(string str);
+        Dictionary<string, string> D = new Dictionary<string, string>();
+
+        private void NewMethod()
+        {
+            Dg dgUIContorol = new Dg(UIContorol);
+
+            List<string> returnList = new List<string>();
+            string str = textBoxXml.Text;
+            string s = "<script[\\s\\S]*?</script>";
+            MatchCollection ms = Regex.Matches(str, s, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            foreach (Match m in ms)
+            {
+                str = str.Replace(m.Value, "");
+            }
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            List<string> strList = new List<string>();
+            strList.Add(".");
+
+            string strPattern = "<([^<>]*?)>";
+            MatchCollection Matches = Regex.Matches(str, strPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            
+            foreach (Match NextMatch in Matches)
+            {
+                if (!NextMatch.Groups[0].Value.EndsWith("/ >") && !NextMatch.Groups[0].Value.EndsWith("/>") && !NextMatch.Groups[0].Value.StartsWith("<!"))
+                {
+                    if (NextMatch.Groups[0].Value.StartsWith("</"))
+                    {
+                        if (NextMatch.Groups[0].Value.Replace("</", "<").ToLower() == strList[strList.Count - 1].ToLower())
+                        {
+                            strList.RemoveAt(strList.Count - 1);
+                        }
+                    }
+                    else
+                    {
+                        string strOldXpath = XpathRow(strList, dic);
+                        string strp = "(<(?<body>[^>]*?) [^>]*?>)|(<(?<body>[^>]*?)>)";
+                        string v = Regex.Matches(NextMatch.Groups[0].Value, strp, RegexOptions.IgnoreCase | RegexOptions.Compiled)[0].Groups["body"].Value.ToLower();
+                        if (v.ToUpper() != "LINK" && v.ToUpper() != "META" && v.ToUpper() != "SCRIPT" && v.ToUpper() != "IMG" && v.ToUpper() != "INPUT" && v.ToUpper() != "FORM")
+                        {
+
+                            AddRowNumber(strOldXpath, "<" + v + ">", dic);
+                            strList.Add("<" + v + ">");
+                            returnList.Add(XpathRow(strList, dic));
+                            //label1.Text = returnList.Last();
+                            try
+                            {
+                                string SelectNodes = hd.DocumentNode.SelectNodes(returnList.Last())[0].InnerHtml;
+                                textBoxValue.Invoke(dgUIContorol, new object[] { returnList.Last() });
+                                D.Add(returnList.Last(), SelectNodes);
+                                //if (D.ContainsKey("./html[1]/body[1]/table[1]/tr[1]/td[1]/table[1]/tr[2]/td[1]/table[1]/tr[1]/td[1]/div[1]/div[1]/fieldset[1]/div[1]"))
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            //listBox1.DataSource = returnList;
+            //listBox1.Items.Add(returnList.Count);
+            if (strList.Count == 1)
+            {
+                toolStripStatusLabel1.Text = "OK";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "False";
+            }
+        }
+        private void UIContorol(string str)
+        {
+            //textBox1.Text = str;
+            listBox1.Items.Add(str);
+            toolStripStatusLabel1.Text = str;
+        }
+
+        private void AddRowNumber(string strOldXpatch, string NewNode, Dictionary<string, int> dic)
+        {
+            if (strOldXpatch == "")
+            {
+                if (!dic.ContainsKey("."))
+                {
+                    dic.Add(".", 0);
+                }
+                else
+                {
+                    dic["."] = 0;
+                }
+                return;
+            }
+            string strPattern = "<(?<body>[^>]*?)>";
+            string v = Regex.Matches(NewNode, strPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled)[0].Groups["body"].Value;
+            if (dic.ContainsKey(strOldXpatch + "/" + v))
+            {
+                dic[strOldXpatch + "/" + v]++;
+            }
+            else
+            {
+                dic.Add(strOldXpatch + "/" + v, 1);
+            }
+        }
+
+        private string XpathRow(List<string> strList, Dictionary<string, int> dic)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var str in strList)
+            {
+                string strPattern = "<(?<body>[^>]*?)>";
+                string v = "";
+                try
+                {
+                    v = Regex.Matches(str, strPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled)[0].Groups["body"].Value;
+
+                    string temp = sb.ToString() + v;
+                    v = v + "[" + dic[temp].ToString() + "]";
+                }
+                catch
+                {
+                    v = str;
+                }
+
+
+                sb.Append(v + "/");
+            }
+            return sb.ToString().TrimEnd('/');
+        }
+
+        HtmlAgilityPack.HtmlDocument hd = new HtmlAgilityPack.HtmlDocument();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string str in D.Where(fun => fun.Value.ToLower().Contains(textBox3.Text.ToLower())).Select(fun => fun.Key))
+            {
+                listBox1.Items.Add(str);
+            }
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                textBoxValue.Text = listBox1.SelectedItem.ToString();
+                string xpath = textBoxValue.Text;
+                button3_Click(null, null);
+                Clipboard.SetText(xpath.Remove(0,1));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxValue.Text = hd.DocumentNode.SelectNodes(textBoxValue.Text)[0].InnerHtml;
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            textBoxXml.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            textBoxValue.Text = hd.DocumentNode.SelectNodes(txtCustomer.Text)[0].InnerHtml;
+        }
+    }
+}
